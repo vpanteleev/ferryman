b816a97faf582243d2b54e600b494f8bbebe5994
v.panteleev <vovapanteleev@gmail.com>
2018-03-04 22:18:34 +0300

    Added TCP server + threading

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ferryman
-Networking service
+Networking Remote Procedure Call service
+
+
+Features
+
+1. Neighbor Discovery Protocol, NDP
+2. Transport Layer Security, TLS
+3. Custom File Transfer Protocol, FTP -- Black Matter Transfer Protocol, BMTP
--- src/server/main.py
+++ src/server/main.py
@@ -1,18 +1,41 @@
 import argparse
 import socket
+from threading import Thread
 
 MAX_BYTES = 65535
 
 
-def server(host, port):
-    main_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    main_socket.bind((host, port))
+def tcp_server(host, port):
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    print('Listen at {}:{}'.format(host, port))
+    # it's possible that some data have no been delivered yet,
+    # or something, so we wait as a cautionous TCP implementation "
+
+    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
+    tcp_socket.bind((host, port))
+    tcp_socket.listen(1)
+
+    print('TCP server listen at {}:{}'.format(host, port))
 
     while True:
-        data, client_address = main_socket.recvfrom(MAX_BYTES)
-        print('Received message: {}, from {}'.format(data, client_address))
+        client_socket, socketname = tcp_socket.accept()
+        print('TCP: Connection accepted -- {}'.format(socketname))
+
+        data = client_socket.recvfrom(MAX_BYTES)
+        print('TCP: Data -- {}'.format(data))
+        client_socket.close()
+
+
+def udp_server(host, port):
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((host, port))
+
+    print('UDP server listen at {}:{}'.format(host, port))
+
+    while True:
+        data, client_address = udp_socket.recvfrom(MAX_BYTES)
+        print('UDP: Received message: {}, from {}'.format(data, client_address))
 
 
 if __name__ == '__main__':
@@ -21,4 +44,6 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=int, default=1060, help='what port to use')
 
     args = parser.parse_args()
-    server(args.host, args.port)
+
+    Thread(target=tcp_server, args=(args.host, args.port)).start()
+    Thread(target=udp_server, args=(args.host, args.port)).start()
