c651262ea3a36fdb433e08121b0de3fe686deef0
v.panteleev <vovapanteleev@gmail.com>
2018-03-05 23:49:33 +0300

    Added dispatcher with simple commands

--- src/server/main.py
+++ src/server/main.py
@@ -1,41 +1,69 @@
 import argparse
 import socket
 from threading import Thread
+import json
 
 MAX_BYTES = 65535
 
 
-def tcp_server(host, port):
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def dispatcher(host, port):
+    dispatcher_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # it's possible that some data have no been delivered yet,
     # or something, so we wait as a cautionous TCP implementation "
 
-    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    dispatcher_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
 
-    tcp_socket.bind((host, port))
-    tcp_socket.listen(1)
+    dispatcher_socket.bind((host, port))
+    dispatcher_socket.listen(1)
 
-    print('TCP server listen at {}:{}'.format(host, port))
-
-    while True:
-        client_socket, socketname = tcp_socket.accept()
-        print('TCP: Connection accepted -- {}'.format(socketname))
-
-        data = client_socket.recvfrom(MAX_BYTES)
-        print('TCP: Data -- {}'.format(data))
-        client_socket.close()
-
-
-def udp_server(host, port):
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind((host, port))
-
-    print('UDP server listen at {}:{}'.format(host, port))
+    print('Dispatcer listen at {}:{}'.format(host, port))
 
     while True:
-        data, client_address = udp_socket.recvfrom(MAX_BYTES)
-        print('UDP: Received message: {}, from {}'.format(data, client_address))
+        client_socket, socketname = dispatcher_socket.accept()
+        print('Dispatcher: Connection accepted -- {}'.format(socketname))
+
+        Thread(target=commander, args=(client_socket,)).start()
+
+
+def commander(client_socket):
+    while client_socket.fileno() != -1:
+
+        raw_data, _ = client_socket.recvfrom(MAX_BYTES)
+        print('Commander: Data -- {}'.format(raw_data))
+
+        parsed_data = parse_command(raw_data)
+        command = parsed_data['command']
+        commands[command](parsed_data, client_socket)
+
+
+def close_connection(client_socket):
+    client_socket.close()
+
+
+def authorize(data, client_socket):
+     user = data['login'] == 'admin' and data['password'] == 'admin'
+     response = 'Kama Kama! Krasaychik!' if user else 'San\' xyu sosi.'
+
+     client_socket.sendall(response.encode('ascii'));
+     print('Commander: Response -- {}'.format(response))
+
+
+def parse_command(raw_data):
+    return json.loads(raw_data.decode('ascii'))
+
+
+def echo(data, client_socket):
+    client_socket.sendall(data.encode('ascii'));
+
+
+def close_connection(_, client_socket):
+    client_socket.close()
+
+
+commands = {'authorize': authorize,
+            "close_connection": close_connection,
+            "echo": echo}
 
 
 if __name__ == '__main__':
@@ -45,5 +73,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    Thread(target=tcp_server, args=(args.host, args.port)).start()
-    Thread(target=udp_server, args=(args.host, args.port)).start()
+    Thread(target=dispatcher, args=(args.host, args.port)).start()
